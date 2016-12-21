a55ea13d8be6c752c5031d7e6fff1a2b812ba019
SarahReyes <siwreyes@gmail.com>
2016-12-21 16:59:47 -0600

    worked on the styling

--- app/components/Main.js
+++ app/components/Main.js
@@ -5,16 +5,15 @@ var ReactDOM = require('react-dom');
 var Main = React.createClass({
 	render: function() {
 		return (
-			<div className="container">
-				<div className="row">
-					<form className="col s12">
-						<div className="row">
-							<div className="input-field col s6">
+			<div className="container" id="movie-search-container">
+				<div className="row" id="movie-search-row">
+					<form className="col s12" id="movie-search-form">
+							<div className="input-field col s12">
 								<input placeholder="search for a movie" id="movie-search" type="text" className="validate" />
 								<label for="movie-search"></label>
 								<a className="waves-effect waves-light btn"><i className="material-icons right">theaters</i>search</a>
 							</div>
-						</div>
+
 					</form>
 				</div>
 			</div>
